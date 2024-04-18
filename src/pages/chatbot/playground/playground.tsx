3e2ee91650930624934f033a7fac6bcacd8c7388
Sai Panindra Santosh Kumar Majji <saipanindrasantoshkumarmajji@Sais-MacBook-Air-3.local>
2024-04-18 14:38:03 -0400

    removing right side info

--- src/pages/chatbot/playground/playground.tsx
+++ src/pages/chatbot/playground/playground.tsx
@@ -10,37 +10,37 @@ export default function Playground() {
   return (
     <BaseAppLayout
       info={
-        <HelpPanel header={<Header variant="h3">Using the chat</Header>}>
-          <p>
-            This chatbot application allows users to ask questions about the RIDE, an MBTA
-          </p>
-          <h3>Settings</h3>
-          <p>
-            You can configure additional settings for the LLM via the setting
-            action at the bottom-right. You can change the Temperature and Top P
-            values to be used for the answer generation. You can also enable and
-            disable streaming mode for those models that support it (the setting
-            is ignored if the model does not support streaming). Turning on
-            Metadata displays additional information about the answer, such as
-            the prompts being used to interact with the LLM and the document
-            passages that might have been retrieved from the RAG storage.
-          </p>
-          <h3>Multimodal chat</h3>
-          <p>
-            If you select a multimodal model (like Anthropic Claude 3), you can
-            upload images to use in the conversation.
-          </p>
-          <h3>Session history</h3>
-          <p>
-            All conversations are saved and can be later accessed via the{" "}
-            <Link to="/chatbot/sessions">Session</Link> in the navigation bar.
-          </p>
-        </HelpPanel>
-      }
-      toolsWidth={300}
-      content={
-       <div>
-      {/* <Chat sessionId={sessionId} /> */}
+      //   <HelpPanel header={<Header variant="h3">Using the chat</Header>}>
+      //     <p>
+      //       This chatbot application allows engineers from MassDOT to quickly search & find relevant information for their projects from the MassDOT Documentation. 
+      //     </p>
+      //     <h3>Settings</h3>
+      //     <p>
+      //       You can configure additional settings for the LLM via the setting
+      //       action at the bottom-right. You can change the Temperature and Top P
+      //       values to be used for the answer generation. You can also enable and
+      //       disable streaming mode for those models that support it (the setting
+      //       is ignored if the model does not support streaming). Turning on
+      //       Metadata displays additional information about the answer, such as
+      //       the prompts being used to interact with the LLM and the document
+      //       passages that might have been retrieved from the RAG storage.
+      //     </p>
+      //     <h3>Multimodal chat</h3>
+      //     <p>
+      //       If you select a multimodal model (like Anthropic Claude 3), you can
+      //       upload images to use in the conversation.
+      //     </p>
+      //     <h3>Session history</h3>
+      //     <p>
+      //       All conversations are saved and can be later accessed via the{" "}
+      //       <Link to="/chatbot/sessions">Session</Link> in the navigation bar.
+      //     </p>
+      //   </HelpPanel>
+      // }
+      // toolsWidth={300}
+      // content={
+      //  <div>
+      // {/* <Chat sessionId={sessionId} /> */}
       <Alert
           statusIconAriaLabel="Info"
           header=""
