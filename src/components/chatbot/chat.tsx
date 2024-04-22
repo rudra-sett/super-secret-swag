339e85494c0bc9c91c59b085fc9ff470c6a7092e
Diane Grant <157505253+Diane-Grant-AI4Impact@users.noreply.github.com>
2024-04-22 16:47:54 -0400

    small update commenting out something abt auth

--- src/components/chatbot/chat.tsx
+++ src/components/chatbot/chat.tsx
@@ -5,7 +5,7 @@ import {
   ChatBotMessageType,
   FeedbackData,
 } from "./types";
-import { Auth } from "aws-amplify";
+// import { Auth } from "aws-amplify";
 import { SpaceBetween, StatusIndicator } from "@cloudscape-design/components";
 import { v4 as uuidv4 } from "uuid";
 import { AppContext } from "../../common/app-context";
