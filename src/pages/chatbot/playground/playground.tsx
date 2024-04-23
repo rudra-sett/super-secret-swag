82ff9dff8564655020a4f9d4f97fdba92df2caa1
Briana Torres <briana@torrez.us>
2024-04-23 16:59:08 -0400

    made local changes

--- src/pages/chatbot/playground/playground.tsx
+++ src/pages/chatbot/playground/playground.tsx
@@ -14,7 +14,7 @@ export default function Playground() {
       info={
         <HelpPanel header={<Header variant="h3">Using the chat</Header>}>
           <p>
-            This chatbot application allows users to ask questions about the RIDE, an MBTA
+            This chatbot application allows users to ask questions about the MassHealth Enrollment Center.
           </p>
           <h3>Settings</h3>
           <p>
