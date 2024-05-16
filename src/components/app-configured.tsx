6b65d07c0ea60b88fce60473c354249adb67dbaf
RachelKahnAI4Impact <157506206+RachelKahnAI4Impact@users.noreply.github.com>
2024-05-16 12:42:11 -0400

    took diane's app configured

--- src/components/app-configured.tsx
+++ src/components/app-configured.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import {  
+import {
   ThemeProvider,
   defaultDarkModeOverride,
 } from "@aws-amplify/ui-react";
@@ -11,25 +11,21 @@ import { Alert, StatusIndicator } from "@cloudscape-design/components";
 import { StorageHelper } from "../common/helpers/storage-helper";
 import { Mode } from "@cloudscape-design/global-styles";
 import "@aws-amplify/ui-react/styles.css";
-
 export default function AppConfigured() {
   const [config, setConfig] = useState<AppConfig | null>(null);
   const [error, setError] = useState<boolean | null>(null);
   //const [authenticated, setAuthenticated] = useState<boolean>(null);
   const [theme, setTheme] = useState(StorageHelper.getTheme());
-  const [configured, setConfigured] = useState<boolean>(false);  
-
+  const [configured, setConfigured] = useState<boolean>(false);
   // this is the authentication provider that Cognito needs
   const federatedIdName : string = "AzureAD-OIDC-MassGov";
-
   //trigger authentication state when needed
   useEffect(() => {
-<<<<<<< Updated upstream
     (async () => {
-      try {     
+      try {
         const result = await fetch("/aws-exports.json");
         const awsExports = await result.json();
-        Amplify.configure(awsExports);   
+        Amplify.configure(awsExports);
         setConfigured(true);
         // const currentUser = await Auth.currentAuthenticatedUser();
         // // console.log("Authenticated user:", currentUser);
@@ -41,34 +37,14 @@ export default function AppConfigured() {
         //setAuthenticated(false);
       }
     })();
-=======
-    // (async () => {
-    //   try {     
-    //     const result = await fetch("/aws-exports.json");
-    //     const awsExports = await result.json();
-    //     Amplify.configure(awsExports);   
-    //     setConfigured(true);
-    //     const currentUser = await Auth.currentAuthenticatedUser();
-    //     // console.log("Authenticated user:", currentUser);
-    //     setAuthenticated(true);
-    //     // console.log(authenticated);
-    //     setConfig(awsExports);
-    //   } catch (e) {
-    //     console.error("Authentication check error:", e); ALAYNA COMMENTED OUT FOR LOCAL DEPLOYMENT 
-    //     setAuthenticated(false);
-    //   }
-    // })();
->>>>>>> Stashed changes
   }, []);
-  
   // // whenever the authentication state changes, if it's changed to un-authenticated, re-verify
-  // useEffect(() => {  
+  // useEffect(() => { 
   //   if (!authenticated && configured) {
   //     console.log("No authenticated user, initiating sign-in.");
   //     Auth.federatedSignIn({ customProvider: federatedIdName });
   //   }
   // }, [authenticated]);
-
   // dark/light theme
   useEffect(() => {
     const observer = new MutationObserver((mutations) => {
@@ -81,7 +57,6 @@ export default function AppConfigured() {
             document.documentElement.style.getPropertyValue(
               "--app-color-scheme"
             );
-
           const mode = newValue === "dark" ? Mode.Dark : Mode.Light;
           if (mode !== theme) {
             setTheme(mode);
@@ -89,17 +64,14 @@ export default function AppConfigured() {
         }
       });
     });
-
     observer.observe(document.documentElement, {
       attributes: true,
       attributeFilter: ["style"],
     });
-
     return () => {
       observer.disconnect();
     };
   }, [theme]);
-
   // display a loading screen while waiting for the config file to load
   if (!config) {
     if (error) {
@@ -123,7 +95,6 @@ export default function AppConfigured() {
         </div>
       );
     }
-
     return (
       <div
         style={{
@@ -134,11 +105,10 @@ export default function AppConfigured() {
           alignItems: "center",
         }}
       >
-        <StatusIndicator type="loading">Loading</StatusIndicator>        
+        <StatusIndicator type="loading">Loading</StatusIndicator>
       </div>
     );
   }
-
   // the main app - only display it when authenticated
   return (
     <AppContext.Provider value={config}>
@@ -154,3 +124,10 @@ export default function AppConfigured() {
     </AppContext.Provider>
   );
 }
+
+
+
+
+
+
+
