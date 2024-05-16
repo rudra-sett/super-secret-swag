3db434505ac8b9371fd109dc7c648692631d4e2a
RachelKahnAI4Impact <157506206+RachelKahnAI4Impact@users.noreply.github.com>
2024-05-16 14:16:08 -0400

    rachel made it work

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -3,26 +3,21 @@ export class Utils {
   // static isDevelopment() {
   //   return import.meta.env.MODE === "development";
   // }
-
   // eslint-disable-next-line @typescript-eslint/ban-types
   static isFunction(value: unknown): value is Function {
     return typeof value === "function";
   }
-
   static classNames(...classes: string[]) {
     return classes.filter(Boolean).join(" ");
   }
-
   static generateUUID() {
     if (crypto && crypto.randomUUID) {
       return crypto.randomUUID();
     }
-
     if (crypto && crypto.getRandomValues) {
       console.log(
         "crypto.randomUUID is not available using crypto.getRandomValues"
       );
-
       return ("" + [1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(
         /[018]/g,
         (ch) => {
@@ -34,7 +29,6 @@ export class Utils {
         }
       );
     }
-
     console.log("crypto is not available");
     let date1 = new Date().getTime();
     let date2 =
@@ -42,7 +36,6 @@ export class Utils {
         performance.now &&
         performance.now() * 1000) ||
       0;
-
     return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
       /[xy]/g,
       function (c) {
@@ -54,80 +47,62 @@ export class Utils {
           r = (date2 + r) % 16 | 0;
           date2 = Math.floor(date2 / 16);
         }
-
         return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
       }
     );
   }
-
   static delay(delay: number) {
     return new Promise((resolve) => {
       setTimeout(resolve, delay);
     });
   }
-
   static findElementInParents(element: HTMLElement | null, tagName: string) {
     let current: HTMLElement | null = element;
     while (current) {
       if (current.tagName === tagName) {
         return current;
       }
-
       current = current.parentElement;
     }
-
     return null;
   }
-
   static getErrorMessage(error: any) {
     if (error.errors) {
       return error.errors.map((e: any) => e.message).join(", ");
     }
-
     return "Unknown error";
   }
-
   static urlSearchParamsToRecord(
     params: URLSearchParams
   ): Record<string, string> {
     const record: Record<string, string> = {};
-
     for (const [key, value] of params.entries()) {
       record[key] = value;
     }
-
     return record;
   }
-
   static bytesToSize(bytes: number): string {
     const sizes: string[] = ["Bytes", "KB", "MB", "GB", "TB", "PB"];
-
     if (bytes === 0) return "0 MB";
     const i: number = parseInt(
       Math.floor(Math.log(bytes) / Math.log(1024)).toString()
     );
-
     const sizeStr = i >= sizes.length ? "" : sizes[i];
     return Math.round(bytes / Math.pow(1024, i)) + " " + sizeStr;
   }
-
   static textEllipsis(str: string, maxLength: number): string {
     if (str.length <= maxLength) return str;
     return str.substring(0, maxLength - 3) + "...";
   }
-
   static isValidURL(value: string) {
     if (value.length === 0 || value.indexOf(" ") !== -1) {
       return false;
     }
-
     const result = value.match(
       /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi
     );
-
     return result !== null;
   }
-
   // static async authenticate(): Promise<string> {
   //   try {
   //     // const currentSession = await Auth.currentSession();
@@ -136,35 +111,32 @@ export class Utils {
   //     // console.log('ID token:', currentSession.getAccessToken().getJwtToken());
   //     // console.log(Auth.currentSession())
   //     // console.log(await Auth.currentSession())
-<<<<<<< Updated upstream
   //     //const currentUser = await Auth.currentAuthenticatedUser()
-=======
-  //     const currentUser = await Auth.currentAuthenticatedUser()
->>>>>>> Stashed changes
   //     // currentUser.
   //     // console.log(currentUser);
   //     // console.log(currentSession);
   //     // token = 'Bearer ' + currentUser.signInUserSession.idToken.jwtToken
-      
   //     // for some reason
-<<<<<<< Updated upstream
-  //     //token = currentUser.signInUserSession.idToken.jwtToken //currentSession.getAccessToken().getJwtToken(); 
-=======
-  //     token = currentUser.signInUserSession.idToken.jwtToken //currentSession.getAccessToken().getJwtToken(); 
->>>>>>> Stashed changes
-
+  //     //token = currentUser.signInUserSession.idToken.jwtToken //currentSession.getAccessToken().getJwtToken();
   //     // currentUser.idToken.jwtToken
-      
   //     // console.log("new token:", token)
   //     // return currentSession.getAccessToken().getJwtToken();
   //     return token
   //   } catch (error) {
   //     console.error('Error getting current user session:', error);
   //     throw new Error('Authentication failed');
-<<<<<<< Updated upstream
   //   }
-=======
-  //   } ALAYNA COMMENTED OUT 
->>>>>>> Stashed changes
   // }
 }
+
+
+
+
+
+
+
+
+
+
+
+
