cb061326298894a04a836cac0ae02d6f88a0b40a
Diane Grant <98796743+Grant-diane@users.noreply.github.com>
2024-05-28 13:01:33 -0400

    adding line breaks to the modal

--- src/components/chatbot/chat.tsx
+++ src/components/chatbot/chat.tsx
@@ -157,12 +157,10 @@ export default function Chat(props: { sessionId?: string }) {
         }
         header="Welcome to the Massachusetts EEA Grants Navigator!"
       >
-        Discover the grants offered by the EEA from Agriculture, Energy, Environmental, and more!
-
-        - Enter your search term. For example: "Composting", into the search bar and see which grant programs relate to composting.
-
+        Discover the grants offered by the EEA from Agriculture, Energy, Environmental, and more!<br />
+        - Enter your search term. For example: "Composting", into the search bar and see which grant programs relate to composting.<br />
         - Select your organization before searching so the results are tailored to you!
-        
+
       </Modal>
      </div>
       <SpaceBetween direction="vertical" size="m">
