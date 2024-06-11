4a77c326b8340edaa862c72b384698ac23fcfa96
Diane Grant <98796743+Grant-diane@users.noreply.github.com>
2024-06-11 16:34:12 -0400

    adding footer, moving the content to the top, and clear search

--- src/components/chatbot/chat-input-panel.tsx
+++ src/components/chatbot/chat-input-panel.tsx
@@ -3,7 +3,6 @@ import {
   Container,
   SpaceBetween,
   Spinner,
-  StatusIndicator,
   Box,
   Select,
   SelectProps
@@ -13,7 +12,6 @@ import {
   SetStateAction,
   useContext,
   useEffect,
-  useLayoutEffect,
   useRef,
   useState,
 } from "react";
@@ -23,34 +21,22 @@ import SpeechRecognition, {
 } from "react-speech-recognition";
 import TextareaAutosize from "react-textarea-autosize";
 import { ReadyState } from "react-use-websocket";
-// import WebSocket from 'ws';
 import { ApiClient } from "../../common/api-client/api-client";
 import { AppContext } from "../../common/app-context";
-// import { OptionsHelper } from "../../common/helpers/options-helper";
-// import { StorageHelper } from "../../common/helpers/storage-helper";
-// import { API } from "aws-amplify";
-// import { GraphQLSubscription, GraphQLResult } from "@aws-amplify/api";
-// import { Model, ReceiveMessagesSubscription, Workspace } from "../../API";
-// import { LoadingStatus, ModelInterface } from "../../common/types";
 import styles from "../../styles/chat.module.scss";
-// import ConfigDialog from "./config-dialog";
-// import ImageDialog from "./image-dialog";
 import {
   ChatBotConfiguration,
   ChatBotHistoryItem,
-  ChatBotMessageResponse,
   ChatBotMessageType,
   ChatInputState,
   ImageFile,
 } from "./types";
-// import { sendQuery } from "../../graphql/mutations";
 import {
   // getSelectedModelMetadata,
   getSignedUrl,
   updateMessageHistoryRef,
   assembleHistory
 } from "./utils";
-// import { receiveMessages } from "../../graphql/subscriptions";
 import { Utils } from "../../common/utils";
 import { SessionRefreshContext } from "../../common/session-refresh-context"
 import { useNotifications } from "../notif-manager";
@@ -159,6 +145,7 @@ export default function ChatInputPanel(props: ChatInputPanelProps) {
     ReadyState.OPEN
   );
   const messageHistoryRef = useRef<ChatBotHistoryItem[]>([]);
+  const [dropdownOpen, setDropdownOpen] = useState(true);
 
   useEffect(() => {
     messageHistoryRef.current = props.messageHistory;
@@ -184,8 +171,6 @@ export default function ChatInputPanel(props: ChatInputPanelProps) {
       let receivedData = '';
 
       messageHistoryRef.current = [
-        ...messageHistoryRef.current,
-
         {
           type: ChatBotMessageType.Human,
           content: messageToSend,
@@ -200,6 +185,7 @@ export default function ChatInputPanel(props: ChatInputPanelProps) {
           content: receivedData,
           metadata: {},
         },
+        ...messageHistoryRef.current
       ];
       props.setMessageHistory(messageHistoryRef.current);
 
@@ -278,7 +264,7 @@ export default function ChatInputPanel(props: ChatInputPanelProps) {
         // console.log(data.data);
         // Update the chat history state with the new message        
         messageHistoryRef.current = [
-          ...messageHistoryRef.current.slice(0, -2),
+          // ...messageHistoryRef.current.slice(0, -2),
 
           {
             type: ChatBotMessageType.Human,
@@ -294,6 +280,7 @@ export default function ChatInputPanel(props: ChatInputPanelProps) {
             content: receivedData,
             metadata: sources,
           },
+          ...messageHistoryRef.current.slice(2)
         ];
         // console.log(messageHistoryRef.current)
         props.setMessageHistory(messageHistoryRef.current);
@@ -320,6 +307,10 @@ export default function ChatInputPanel(props: ChatInputPanelProps) {
     }
   };
 
+  const handleClearSearch = () => {
+    setState({ value: "", systemPrompt: defaultPrompt });
+  };
+
   const connectionStatus = {
     [ReadyState.CONNECTING]: "Connecting",
     [ReadyState.OPEN]: "Open",
@@ -333,7 +324,6 @@ export default function ChatInputPanel(props: ChatInputPanelProps) {
       "https://unpkg.com/@massds/mayflower-assets@13.1.0/css/global.min.css",
       "https://unpkg.com/@massds/mayflower-assets@13.1.0/css/layout.min.css",
       "https://unpkg.com/@massds/mayflower-assets@13.1.0/css/brand-banner.min.css",
-      "https://unpkg.com/@massds/mayflower-assets@13.1.0/css/footer-slim.css",
     ];
 
     stylesheets.forEach((href) => {
@@ -344,61 +334,13 @@ export default function ChatInputPanel(props: ChatInputPanelProps) {
     });
   };
 
-  const addFooterOnce = () => {
-    if (!document.getElementById('footer')) {
-      const footerHTML = `
-      <footer data-nosnippet="true" class="ma__footer-new" id="footer">
-        <div class="ma__footer-new__container">
-          <div class="ma__footer-new__logo">
-            <a href="/" title="Mass.gov home page">
-              <img
-                src="https://unpkg.com/@massds/mayflower-assets@13.1.0/static/images/logo/stateseal.png"
-                alt="Massachusetts State Seal"
-                width="100"
-                height="100"
-              />
-            </a>
-          </div>
-          <div class="ma__footer-new__content">
-            <nav class="ma__footer-new__navlinks" aria-label="site navigation">
-              <div index="0">
-                <a href="https://www.mass.gov/topics/massachusetts-topics">
-                  All Topics
-                </a>
-              </div>
-              <div index="1">
-                <a href="https://www.mass.gov/massgov-site-policies">Site Policies</a>
-              </div>
-              <div index="2">
-                <a href="https://www.mass.gov/topics/public-records-requests">
-                  Public Records Requests
-                </a>
-              </div>
-            </nav>
-            <div class="ma__footer-new__copyright">
-              <div class="ma__footer-new__copyright--bold">
-                © 2024 Commonwealth of Massachusetts.
-              </div>
-              <span>
-                Mass.gov® is a registered service mark of the Commonwealth of
-                Massachusetts.
-              </span>
-              <a href="https://www.mass.gov/privacypolicy">Mass.gov Privacy Policy</a>
-            </div>
-          </div>
-        </div>
-      </footer>
-    `;
-    const footer = document.createElement("div");
-    footer.innerHTML = footerHTML;
-    document.body.appendChild(footer);
-    }
-  };
-
   useEffect(() => {
     addMayflowerStyles();
-    addFooterOnce();
   }, []);
+
+  const handleDropdownToggle = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
 
   const selectPrompt = (type: string) => {
     setActiveButton(type);
@@ -429,38 +371,90 @@ export default function ChatInputPanel(props: ChatInputPanelProps) {
   ];
 
   return (
-    <SpaceBetween direction="vertical" size="l">
-      <Container>
-        <div className={`${styles.input_textarea_container} input_textarea_container`}>
-            <span style={{ fontFamily: 'Calibri, sans-serif', fontSize: 18, marginRight: '8px' }}>I am a</span>
-            <Select
-              options={typeOptions}
-              selectedOption={selectedType}
-              onChange={({ detail }) => {
-                setSelectedType(detail.selectedOption);
-                selectPrompt(detail.selectedOption.value || 'General');
-              }}
-              placeholder="Select type"
+    <div style={{ display: 'flex', flexDirection: 'column'}}>
+      <footer style={{ position: 'fixed', bottom: 0, width: '100%' }}>
+        <div className="ma__brand-banner ma__brand-banner--c-primary-alt-bg-light">
+          <button
+            className="ma__brand-banner-container"
+            aria-controls="ma__brand-banner-content"
+            aria-expanded={dropdownOpen}
+            onClick={handleDropdownToggle}
+          >
+            <img
+              className="ma__brand-banner-logo"
+              src="https://unpkg.com/@massds/mayflower-assets@13.1.0/static/images/logo/stateseal.png"
+              alt="Massachusetts State Seal"
             />
-            <span style={{ fontFamily: 'Calibri, sans-serif', fontSize: 18, marginLeft: '8px' }}>looking for grants for</span>
-            <TextareaAutosize
-              className={`${styles.input_textarea} input_textarea`}
-              maxRows={6}
-              minRows={1}
-              spellCheck={true}
-              autoFocus
-              onChange={(e) =>
-                setState((state) => ({ ...state, value: e.target.value }))
-              }
-              onKeyDown={(e) => {
-                if (e.key == "Enter" && !e.shiftKey) {
-                  e.preventDefault();
-                  handleSendMessage();
-                }
-              }}
-              value={state.value}
-              placeholder={"Enter Search ex. \"Grants for new farmers\""}
-            />
+            <span className="ma__brand-banner-text">
+              <span>An official website of the Commonwealth of Massachusetts</span>
+              <span>   </span>
+              <span className="ma__brand-banner-button ma__button-icon ma__icon-small ma__button-icon--quaternary ma__button-icon--c-primary-alt">
+                <span>Here's how you know</span>
+                <svg aria-hidden="true" width="1em" height="1em" viewBox="0 0 59 38" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M29.414 37.657L.344 8.586 8.828.102l20.586 20.584L50 .1l8.484 8.485-29.07 29.072"></path>
+                </svg>
+              </span>
+            </span>
+          </button>
+          {dropdownOpen && (
+            <ul className="ma__brand-banner-expansion" id="ma__brand-banner-content">
+              <li className="ma__brand-banner-expansion-item">
+                <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 16 16" fill="#14558F">
+                  <path d="M8.73 1.592V0H7.27v1.592c-2.06.364-3.63 2.239-3.63 4.503h8.72c0-2.264-1.57-4.14-3.63-4.503zm6.54 9.837h-2.18V8.38c0-.42-.32-.762-.73-.762H3.64c-.41 0-.73.341-.73.762v3.048H.73c-.4 0-.73.34-.73.761v3.048c0 .421.33.762.73.762h14.54c.4 0 .73-.34.73-.762v-3.047c0-.421-.33-.762-.73-.762zM2.91 14.476H1.45v-1.524h1.46zm2.91 0H4.36v-1.524h1.46zm0-3.81H4.36V9.144h1.46zm2.91 3.81H7.27v-1.524h1.46zm0-3.81H7.27V9.144h1.46zm2.91 3.81h-1.46v-1.524h1.46zm0-3.81h-1.46V9.144h1.46zm2.91 3.81h-1.46v-1.524h1.46z"></path>
+                </svg>
+                <div className="ma__brand-banner-expansion-item-content">
+                  <p>Official websites use .mass.gov</p>
+                  <p>A .mass.gov website belongs to an official government organization in Massachusetts.</p>
+                </div>
+              </li>
+              <li className="ma__brand-banner-expansion-item">
+                <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21 25" aria-hidden="true" fill="#388557">
+                  <path d="M10.399 0a6.491 6.491 0 016.487 6.258l.004.233-.001 3.853h2.077c.952 0 1.724.773 1.724 1.725v11.207c0 .952-.772 1.724-1.724 1.724H1.724A1.724 1.724 0 010 23.276V12.069c0-.952.772-1.724 1.724-1.724l2.184-.001V6.491l.004-.233A6.491 6.491 0 0110.4 0zm0 1.517A4.974 4.974 0 005.43 6.275l-.005.216v3.853h9.947V6.491l-.004-.216a4.974 4.974 0 00-4.97-4.758z" fillRule="evenodd"></path>
+                </svg>
+                <div className="ma__brand-banner-expansion-item-content">
+                  <p>Secure websites use HTTPS certificate</p>
+                  <p>A lock icon (
+                    <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21 25" aria-hidden="true">
+                      <path d="M10.399 0a6.491 6.491 0 016.487 6.258l.004.233-.001 3.853h2.077c.952 0 1.724.773 1.724 1.725v11.207c0 .952-.772 1.724-1.724 1.724H1.724A1.724 1.724 0 010 23.276V12.069c0-.952.772-1.724 1.724-1.724l2.184-.001V6.491l.004-.233A6.491 6.491 0 0110.4 0zm0 1.517A4.974 4.974 0 005.43 6.275l-.005.216v3.853h9.947V6.491l-.004-.216a4.974 4.974 0 00-4.97-4.758z" fillRule="evenodd"></path>
+                    </svg>
+                  ) or https:// means you’ve safely connected to the official website. Share sensitive information only on official, secure websites.</p>
+                </div>
+              </li>
+            </ul>
+          )}
+        </div>
+      </footer>
+      <main style={{ flex: 1, overflowY: 'auto' }}>
+        <SpaceBetween direction="vertical" size="xs">
+          <Container>
+            <div className={`${styles.input_textarea_container} input_textarea_container`}>
+              <span style={{ fontFamily: 'Calibri, sans-serif', fontSize: 18, marginRight: '8px' }}>I am a</span>
+              <Select
+                options={typeOptions}
+                selectedOption={selectedType}
+                onChange={({ detail }) => {
+                  setSelectedType(detail.selectedOption);
+                  selectPrompt(detail.selectedOption.value || 'General');
+                }}
+                placeholder="Select type"
+              />
+              <span style={{ fontFamily: 'Calibri, sans-serif', fontSize: 18, marginLeft: '8px' }}>looking for grants for</span>
+              <TextareaAutosize
+                className={`${styles.input_textarea} input_textarea`}
+                maxRows={6}
+                minRows={1}
+                spellCheck={true}
+                autoFocus
+                onChange={(e) => setState((state) => ({ ...state, value: e.target.value }))}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter" && !e.shiftKey) {
+                    e.preventDefault();
+                    handleSendMessage();
+                  }
+                }}
+                value={state.value}
+                placeholder={"Enter Search ex. \"Grants for new farmers\""}
+              />
               <Button
                 disabled={
                   readyState !== ReadyState.OPEN ||
@@ -481,30 +475,26 @@ export default function ChatInputPanel(props: ChatInputPanelProps) {
                   "Search"
                 )}
               </Button>
-        </div>
-      </Container>
-      <div className={styles.info_bar}>
-        <div style={{ flex: 1, display: 'flex', justifyContent: 'center' }}>
-          <div style={{ marginTop: '30px' }}>
-            <AIWarning />
+              <Button
+                onClick={handleClearSearch}
+                iconAlign="left"
+                iconName="close"
+                variant="link"
+                style={{ marginLeft: '8px' }}
+              >
+                Clear Search
+              </Button>
+            </div>
+          </Container>
+          <div className={styles.info_bar}>
+            <div style={{ flex: 1, display: 'flex', justifyContent: 'center' }}>
+              <div style={{ marginTop: '30px' }}>
+                <AIWarning />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </SpaceBetween>
+        </SpaceBetween>
+      </main>
+    </div>
   );
-}
-
-// Ensure the footer is at the bottom with CSS
-const style = document.createElement('style');
-style.innerHTML = `
-  .chat-input-panel-container {
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .ma__footer-new {
-    margin-top: auto;
-  }
-`;
-document.head.appendChild(style);
+}  
