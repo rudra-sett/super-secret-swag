2a2739e02e6242151785907296a13ce60afec170
Diane Grant <98796743+Grant-diane@users.noreply.github.com>
2024-06-14 13:50:46 -0400

    added functionality of bottom brand banner

--- src/components/chatbot/chat.tsx
+++ src/components/chatbot/chat.tsx
@@ -14,6 +14,7 @@ import Modal from "@cloudscape-design/components/modal";
 import ChatInputPanel, { ChatScrollState } from "./chat-input-panel";
 import styles from "../../styles/chat.module.scss";
 import Box from '@cloudscape-design/components/box';
+import BrandBanner from "./BrandBanner"; // Import the new Dropdown component
 
 const DefaultButton = () => {
   return <Button variant="link">Other</Button>;
@@ -47,7 +48,6 @@ export default function Chat(props: { sessionId?: string }) {
     id: props.sessionId ?? uuidv4(),
     loading: typeof props.sessionId !== "undefined",
   });
-
   const [configuration, setConfiguration] = useState<ChatBotConfiguration>(
     () => ({
       streaming: true,
@@ -168,32 +168,9 @@ export default function Chat(props: { sessionId?: string }) {
       <SpaceBetween direction="vertical" size="m">
 
       <SpaceBetween direction="vertical" size="m"></SpaceBetween>
-        {/* {messageHistory.map((message, idx) => (
-          <ChatMessage
-            key={idx}
-            message={message}
-            showMetadata={configuration.showMetadata}
-            onThumbsUp={() => handleFeedback(1,idx, message)}
-            onThumbsDown={(feedbackTopic : string, feedbackType : string, feedbackMessage: string) => handleFeedback(0,idx, message,feedbackTopic, feedbackType, feedbackMessage)}
-          />
-        ))} */}
       </SpaceBetween>
-      {/* <div className={styles.welcome_text}>
-        {session?.loading && (
-          <center>
-            <StatusIndicator type="loading">Loading session</StatusIndicator>
-          </center>
-        )}
-      </div> */}
       <div className={styles.input_container}>
         <SpaceBetween direction="horizontal" size="l">
-            {/* <div style={{ display: 'flex', justifyContent: 'center'}}>
-              <FarmButton/>
-              <BusinessButton/>
-              <NonprofitButton/>
-              <TownButton/>
-              <DefaultButton/>
-            </div> */}
           </SpaceBetween> 
         <SpaceBetween direction="vertical" size="s">
           <ChatInputPanel
@@ -218,8 +195,9 @@ export default function Chat(props: { sessionId?: string }) {
           </SpaceBetween>
         </div>
       </div>
+      <footer style={{ position: 'fixed', bottom: 0, width: '100%'}}>
+        <BrandBanner />
+      </footer>
     </div>
   );
-  
-
-}
+};
